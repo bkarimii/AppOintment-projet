3b45dac19decf455d1fa534a04260a629ec1d095
fikretellek <fikretellek@icloud.com>
2024-10-10 23:10:11 +0100

    getLocation function implemented and tested manually, automation testing is not working due to database conflict

---
+++ api/functions/getLocation/getLocation.test.js
@@ -0,0 +1,16 @@
+import { getLocation } from "./getLocation";
+
+describe("getLocation function", () => {
+	describe("when existing crs code given", () => {
+		it("returns matching long&lat value in appropriate format", async () => {
+			const expectedResponse = {
+				location: {
+					latLng: { latitude: 53.47671998, longitude: -2.228977818 },
+				},
+			};
+			expect(await getLocation("MAN")).toBe(expectedResponse);
+		});
+	});
+});
+
+// this test is not working because test database is different than production env database
