8241f4ab544187bd846df2036c265a1e1f90f403
fikretellek <fikretellek@icloud.com>
2024-10-12 00:08:58 +0100

    new page created for new form

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 
-import "./pages/DisplayComponent.css";
-import DisplayTravelResults from "./pages/DisplayTravelResults.jsx";
+import "./App.css";
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import NewMeeting from "./pages/NewMeeting.jsx";
+
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<DisplayTravelResults />} />
+			<Route path="/" element={<Home />} />
+			<Route path="/new-meeting" element={<NewMeeting />} />
+			<Route path="/nested/about/path" element={<About />} />
 		</Routes>
 	);
 }
