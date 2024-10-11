c7bff05fe09d0bfa171df0e61902c1c4efd4dca8
fikretellek <fikretellek@icloud.com>
2024-10-12 00:08:26 +0100

    api implemented to get the station list

--- api/api.js
+++ api/api.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
+import db from "./db.js";
 import messageRouter from "./messages/messageRouter.js";
 
 const api = Router();
 
 api.use("/message", messageRouter);
+
+api.get("/station-list", async (req, res) => {
+	const stations = await db.query(
+		"SELECT station_name, crs_code FROM uk_stations",
+	);
+	res.status(200).json(stations.rows);
+});
 
 export default api;
