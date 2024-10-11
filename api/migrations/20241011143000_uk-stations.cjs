649acc02276a9314b328d06fe86a150d1cfcb967
fikretellek <fikretellek@icloud.com>
2024-10-11 21:54:17 +0100

    test implemented

---
+++ api/migrations/20241011143000_uk-stations.cjs
@@ -0,0 +1,32 @@
+/**
+ * Template for
+ * {@link https://salsita.github.io/node-pg-migrate/#/migrations?id=defining-migrations defining migrations}.
+ *
+ * @type {{
+ *   down: (pgm: import("node-pg-migrate").MigrationBuilder) => void | Promise<void>;
+ *   shorthands: Record<string, import("node-pg-migrate").ColumnDefinition>;
+ *   up: (pgm: import("node-pg-migrate").MigrationBuilder) => void | Promise<void>;
+ * }}
+ */
+const migration = {
+	up(pgm) {
+		pgm.createTable("uk_stations", {
+			station_id: { notNull: true, primaryKey: true, type: "int8" },
+			station_name: { notNull: true, type: "text" },
+			sixteen_character_name: { notNull: true, type: "text" },
+			longitude: { notNull: true, type: "float8" },
+			latitude: { notNull: true, type: "float8" },
+			crs_code: { notNull: true, type: "text" },
+		});
+		pgm.sql(
+			"INSERT INTO uk_stations (station_id, station_name, sixteen_character_name, longitude, latitude, crs_code) VALUES (1524, 'Manchester Piccadilly','MANCHESTER PIC',-2.228977818,53.47671998,'MAN')",
+		);
+	},
+
+	down(pgm) {
+		pgm.dropTable("uk_stations");
+	},
+	shorthands: undefined,
+};
+
+module.exports = migration;
