7e05f6ebe9c9eac04fb72c719c2434487d39d21d
fikretellek <fikretellek@icloud.com>
2024-10-14 20:07:25 +0100

    commented lines deleted

--- api/api.js
+++ api/api.js
@@ -1,14 +1,9 @@
 import { Router } from "express";
 
 import messageRouter from "./messages/messageRouter.js";
-// import { getLocation } from "./functions/getLocation/getLocation.js";
 
 const api = Router();
 
 api.use("/message", messageRouter);
-
-// api.get("/test", async (req, res) => {
-// 	res.send(await getLocation("MAN"));
-// });
 
 export default api;
