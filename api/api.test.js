9c25442740bdf30dc768640255622827ed81523c
karimi <behrouzkarimi1993@gmail.com>
2024-10-15 15:35:39 +0100

    unit test for the endpoint

--- api/api.test.js
+++ api/api.test.js
@@ -27,26 +27,31 @@ describe("/api", () => {
 			intervalTime: "15",
 		};
 
-		it("returns an array", async () => {
+		it("returns an array of travel stats", async () => {
 			const response = await request(app).post("/api/compute-route").send(body);
+
 			expect(response.status).toBe(200);
+
+			// Validate the response body structure
+			expect(Array.isArray(response.body)).toBe(true); // Check if response body is an array
+
 			expect(response.body).toEqual(
 				expect.arrayContaining([
 					expect.objectContaining({
-						meetingTime: expect.any(String),
-						maxTravelTime: expect.any(Number),
-						minTravelTime: expect.any(Number),
-						averageTravelTime: expect.any(Number),
-						earliestArrival: expect.any(String),
-						latestArrival: expect.any(String),
-						earliestDeparture: expect.any(String),
-						latestDeparture: expect.any(String),
-						medianArrivalTime: expect.any(String),
-						difficultTravels: expect.arrayContaining(),
-						tooLongTravel: expect.arrayContaining(),
+						meetingTime: expect.any(String), // "2024-10-13T10:00:00.000Z"
+						maxTravelTime: expect.any(Number), //  8.766666666666667
+						minTravelTime: expect.any(Number), //  8.766666666666667
+						averageTravelTime: expect.any(Number), // 8.766666666666667
+						earliestArrival: expect.any(String), // "09:40:00"
+						latestArrival: expect.any(String), // "09:40:00"
+						earliestDeparture: expect.any(String), // "01:20:00"
+						latestDeparture: expect.any(String), // "01:20:00"
+						medianArrivalTime: expect.any(String), // "09:40:00"
+						difficultTravels: expect.arrayContaining([]), // Should be an array
+						tooLongTravel: expect.arrayContaining([]), // Should be an array
 					}),
 				]),
 			);
 		});
-	}, 10000);
+	}, 20000);
 });
