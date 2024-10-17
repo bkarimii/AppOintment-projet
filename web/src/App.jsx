18c161d56bd00a9d673f9124ae30040d6bb3e627
David Christensen <dc@cpsd.co.uk>
2024-10-17 16:13:57 +0100

    Make lint happy with the conflict resolve

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,14 +5,12 @@ import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import NewMeeting from "./pages/NewMeeting.jsx";
 
-
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/new-meeting" element={<NewMeeting />} />
 			<Route path="/nested/about/path" element={<About />} />
-
 		</Routes>
 	);
 }
