52c7d8082bfe987380b80a3f658fdb7b76a86e7d
karimi <behrouzkarimi1993@gmail.com>
2024-10-18 17:27:25 +0100

    fixing api.test

--- api/api.test.js
+++ api/api.test.js
@@ -1,39 +1,35 @@
 import request from "supertest";
 
 import app from "./app.js";
-
 describe("/api", () => {
 	describe("POST /compute-route", () => {
 		const body = {
-			origins: [
-				{
-					city: "Liverpool",
-					location: {
-						latLng: {
-							latitude: 53.40461053,
-							longitude: -2.979159936,
-						},
-					},
-				},
-			],
-			destination: {
-				latitude: 52.1945746,
-				longitude: 0.137554552,
-			},
-			meetingRange: {
-				startingTime: "2024-10-13T10:00:00Z",
-				endingTime: "2024-10-13T13:00:00Z",
-			},
-			intervalTime: "15",
+			meetingStation: "ABW",
+			meetingDate: "2024-10-18",
+			earliestStartTime: "05:29",
+			latestStartTime: "06:29",
+			attendees: ["MAN"],
+			intervalTime: 15,
 		};
-
-		it("returns an array of travel stats", async () => {
+		it("returns an array", async () => {
 			const response = await request(app).post("/api/compute-route").send(body);
-
-			expect(response.status).toBe(200);
-
 			// Validate the response body structure
 			expect(Array.isArray(response.body)).toBe(true); // Check if response body is an array
+		}, 30000);
+	});
+	describe("POST /compute-route1", () => {
+		const body = {
+			meetingStation: "ABW",
+			meetingDate: "2024-10-18",
+			earliestStartTime: "05:29",
+			latestStartTime: "06:29",
+			attendees: ["MAN"],
+			intervalTime: 15,
+		};
+		it("returns an array of travel stats", async () => {
+			const response = await request(app).post("/api/compute-route").send(body);
+			expect(response.status).toBe(200);
+			// Validate the response body structure
 		});
 	});
 });
