1f94f362a092656d68ea3590221c645cef35584e
karimi <behrouzkarimi1993@gmail.com>
2024-10-24 19:04:14 +0100

    report is ready

--- web/src/pages/ReportMaker.jsx
+++ web/src/pages/ReportMaker.jsx
@@ -4,14 +4,22 @@ import { useState } from "react";
 import "./ReportMaker.css";
 
 export function ReportMaker({ arrayOfReport }) {
-	const [inputValueHolder, setInputValueHolder] = useState("");
-	const [searchForAttendee, setSearchForAttendee] = useState(false);
 	const [showGeneralReport, setShowGeneralReport] = useState(true);
+	const [isCopied, setIsCopied] = useState(false);
+
+	// Extract destination from local storage
+	const meetingData = localStorage.getItem("newMeetingData");
+	const parsedMeetingData = JSON.parse(meetingData);
+	const destinationTrainStation = parsedMeetingData.meetingStation;
 
 	const copyReportToClipboard = async (reportText) => {
 		try {
 			await navigator.clipboard.writeText(reportText);
-			alert("Report copied!");
+			// alert("Report copied!");
+			setIsCopied(true);
+			setTimeout(() => {
+				setIsCopied(false);
+			}, 4000);
 		} catch (error) {
 			console.error("Failed to copy: ", error);
 		}
@@ -19,61 +27,9 @@ export function ReportMaker({ arrayOfReport }) {
 
 	const handleCopyButton = (reportText) => {
 		copyReportToClipboard(reportText);
-	};
-
-	const functionToSearchForAttendee = (attendeeName) => {
-		if (attendeeName.length) {
-			const filteredAttendee = arrayOfReport.reduce(
-				(accumulator, meetingSlotTimeObject) => {
-					const attendeeTravelInfo = meetingSlotTimeObject.personalReports;
-
-					const attendeeFound = attendeeTravelInfo.find(
-						(attendee) =>
-							attendee.name.toLowerCase() === attendeeName.toLowerCase(),
-					);
-
-					if (attendeeFound) {
-						accumulator.push({
-							meetingDate: meetingSlotTimeObject.meetingDate,
-							meetingTime: meetingSlotTimeObject.meetingTime,
-							personalReports: attendeeFound,
-						});
-					}
-
-					return accumulator;
-				},
-				[],
-			);
-			return filteredAttendee;
-		}
-		return [];
-	};
-
-	const handleSearchButton = () => {
-		if (inputValueHolder.trim().length === 0) {
-			// If input is empty, show general report
-			alert("Search box is empty! Showing general report.");
-			setSearchForAttendee(false);
-			setShowGeneralReport(true);
-		} else {
-			const results = functionToSearchForAttendee(inputValueHolder);
-			setSearchForAttendee(true);
-			setShowGeneralReport(false);
-			if (results.length > 0) {
-				console.log(JSON.stringify(results));
-			} else {
-				alert("No result found for the search!");
-				setSearchForAttendee(false);
-				setShowGeneralReport(true);
-			}
-		}
-	};
-
-	// Function to clear input box and reset to general report
-	const handleClearInput = () => {
-		setInputValueHolder(""); // Clear the input box
-		setSearchForAttendee(false); // Stop showing search results
-		setShowGeneralReport(true); // Show general report
+		setTimeout(() => {
+			setIsCopied(false);
+		});
 	};
 
 	function generateReportText(arrayOfReport) {
@@ -82,30 +38,60 @@ export function ReportMaker({ arrayOfReport }) {
 
 			const reportText =
 				`Meeting on ${personTravel.meetingDate} at ${personTravel.meetingTime}\n` +
+				`Destination: ${destinationTrainStation}\n` + // Add destination to report text
 				personalReports
 					.map(
 						(personalInfo) =>
-							`Attendee: ${personalInfo.name}, From: ${personalInfo.origin}, Departure: ${personalInfo.departureTime}, Arrival: ${personalInfo.arrivalTime}, Duration: ${personalInfo.approximateTravelTime} minutes`,
+							`Attendee: ${personalInfo.name}, From: ${personalInfo.origin}, ` +
+							`Departure: ${personalInfo.departureTime}, ` +
+							`Arrival: ${personalInfo.arrivalTime}, ` +
+							`Duration: ${personalInfo.approximateTravelTime} minutes`,
 					)
 					.join("\n");
 
 			return (
 				<div key={index} className="report-container">
-					<button onClick={() => handleCopyButton(reportText)}>Copy</button>
-					<h3>
-						Meeting on {personTravel.meetingDate} at {personTravel.meetingTime}
-					</h3>
+					{isCopied ? (
+						<button onClick={() => handleCopyButton(reportText)}>Copied</button>
+					) : (
+						<button onClick={() => handleCopyButton(reportText)}>
+							Copy report
+						</button>
+					)}
+
+					<h3> Meeting Report</h3>
 					{personalReports.map((personalInfo, innerIndex) => (
-						<div key={innerIndex}>
+						<div key={innerIndex} className="each-person-report">
 							<h4>Attendee: {personalInfo.name}</h4>
-							<h5>From: {personalInfo.origin}</h5>
+							<h4>From: {personalInfo.origin}</h4>
 							<p>
-								It is on <b>{personTravel.meetingDate}</b> at{" "}
-								{personTravel.meetingTime} in {personalInfo.origin}. Departure
-								time is {personalInfo.departureTime} and expected to arrive at{" "}
-								{personalInfo.arrivalTime}. It lasts around{" "}
-								{personalInfo.approximateTravelTime} minutes.
+								<strong>Date of Meeting:</strong>{" "}
+								<strong>{personTravel.meetingDate}</strong>
 							</p>
+							<p>
+								<strong>Time of Meeting:</strong>{" "}
+								<strong>{personTravel.meetingTime}</strong>
+							</p>
+							<p>
+								<strong>Departure Location:</strong>{" "}
+								<strong>{personalInfo.origin}</strong>
+							</p>
+							<p>
+								<strong>Travel Details:</strong>
+							</p>
+							<ul>
+								<li>
+									<strong>Departure Time:</strong> {personalInfo.departureTime}
+								</li>
+								<li>
+									<strong>Expected Arrival Time:</strong>{" "}
+									{personalInfo.arrivalTime}
+								</li>
+								<li>
+									<strong>Approximate Duration:</strong>{" "}
+									{personalInfo.approximateTravelTime} minutes
+								</li>
+							</ul>
 						</div>
 					))}
 				</div>
@@ -117,42 +103,7 @@ export function ReportMaker({ arrayOfReport }) {
 
 	return (
 		<>
-			<button>Generate Report</button>
-			<div>
-				<p>Search attendees: </p>
-				<input
-					value={inputValueHolder}
-					onChange={(e) => {
-						setInputValueHolder(e.target.value);
-					}}
-				/>
-				<button onClick={handleSearchButton}>Search</button>
-				<button onClick={handleClearInput}>Clear</button> {/* Clear button */}
-			</div>
-			{/* Conditionally display search results or general report */}
-			<div>
-				{searchForAttendee
-					? functionToSearchForAttendee(inputValueHolder).map(
-							(result, index) => (
-								<div key={index}>
-									<h3>
-										Meeting on {result.meetingDate} at {result.meetingTime}
-									</h3>
-									<div>
-										<h4>Attendee: {result.personalReports.name}</h4>
-										<h5>From: {result.personalReports.origin}</h5>
-										<p>
-											Departure: {result.personalReports.departureTime},
-											Arrival:
-											{result.personalReports.arrivalTime}, Duration:{" "}
-											{result.personalReports.approximateTravelTime} minutes
-										</p>
-									</div>
-								</div>
-							),
-						)
-					: textOfReport}
-			</div>
+			<div>{textOfReport}</div>
 		</>
 	);
 }
