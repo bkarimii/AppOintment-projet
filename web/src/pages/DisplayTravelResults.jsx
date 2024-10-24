463a897ffba6bd22c16b8c600a5e990c9360989f
karimi <behrouzkarimi1993@gmail.com>
2024-10-24 19:08:30 +0100

    report is shown in the table , final stage

--- web/src/pages/DisplayTravelResults.jsx
+++ web/src/pages/DisplayTravelResults.jsx
@@ -1,64 +1,65 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import DisplayDetailOfResults from "./DisplayTravelDetails";
 import { ReportMaker } from "./ReportMaker";
 import Visualise from "./Visualise";
+
 import "./DisplayComponent.css";
 
 function DisplayTravelResults() {
-	const [processedResultsStorage, setProcessedResultsStorage] = useState([]); // No more static data
-	const [processedReport, setProcessedReport] = useState([]); // No more static data
+	const [processedResultsStorage, setProcessedResultsStorage] = useState([]);
+	const [processedReport, setProcessedReport] = useState([]);
 	const [expandedRow, setExpandedRow] = useState(null);
+	const [loading, setLoading] = useState(false); // Loading state
 	const navigate = useNavigate();
-	const url = "/api/compute-route"; // API URL
+	const url = "/api/compute-route";
 
-	// Fetch travel data when the component is mounted
-	useEffect(() => {
-		fetchTravelData(url);
-	}, []);
-
-	function extractDateTime(isoString) {
-		const dateObject = new Date(isoString);
-
-		const date = dateObject.toLocaleDateString("en-GB");
-		const time = dateObject.toLocaleTimeString("en-GB", {
-			hour: "2-digit",
-			minute: "2-digit",
-		});
-
-		return [date, time];
-	}
-
-	// Function to fetch travel data from API
 	const fetchTravelData = async (URL) => {
+		setLoading(true); // Set loading to true when starting to fetch data
 		try {
 			const bodyData = localStorage.getItem("newMeetingData");
-			console.log(bodyData);
 
 			const response = await fetch(URL, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
 				},
-				body: bodyData, // Sending the stored meeting data
+				body: bodyData,
 			});
 
 			if (response.ok) {
 				const totalInformation = await response.json();
-				const result = totalInformation[0]; // Assuming the first part is results
-				const reports = totalInformation[1]; // Assuming the second part is reports
-
+				const result = totalInformation[0];
+				const reports = totalInformation[1];
 				setProcessedResultsStorage(result);
 				setProcessedReport(reports);
-				console.log(reports, "<-------reports from API");
 			} else {
 				console.error("An error happened!", response.status.error);
 			}
 		} catch (error) {
 			console.error(error);
+		} finally {
+			setLoading(false); // Set loading to false when done
 		}
 	};
+
+	useEffect(() => {
+		fetchTravelData(url);
+	}, []);
+
+	const extractDateTime = (isoString) => {
+		const dateObject = new Date(isoString);
+		const date = dateObject.toLocaleDateString("en-GB");
+		const time = dateObject.toLocaleTimeString("en-GB", {
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+		return [date, time];
+	};
+
+	// Function to fetch travel data from API
 
 	const toggleRowExpansion = (index) => {
 		setExpandedRow(expandedRow === index ? null : index); // Toggle the expanded row
@@ -74,66 +75,80 @@ function DisplayTravelResults() {
 			<button onClick={handleGoBackButton}>Go Back</button>
 
 			<div>
-				<div>
-					<table>
-						<thead>
-							<tr>
-								<th>Meeting Date</th>
-								<th>Meeting Time</th>
-								<th>Min Travel Time</th>
-								<th>Average Travel Time</th>
-								<th>Max Travel Time</th>
-								<th>Latest Arrival</th>
-								<th>Earliest Departure</th>
-							</tr>
-						</thead>
-						<tbody>
-							{processedResultsStorage.map((result, index) => (
-								<>
-									<tr key={index} onClick={() => toggleRowExpansion(index)}>
-										<td data-label="Meeting Date">
-											{extractDateTime(result.meetingTime)[0]}
-										</td>
-										<td data-label="Meeting Time">
-											{extractDateTime(result.meetingTime)[1]}
-										</td>
+				{loading ? ( // Conditional rendering for loading indicator
+					<p>
+						<strong>Loading data, please wait...</strong>
+					</p>
+				) : (
+					<div>
+						<table>
+							<thead>
+								<tr>
+									<th>Meeting Date</th>
+									<th>Meeting Time</th>
+									<th>Min Travel Time</th>
+									<th>Average Travel Time</th>
+									<th>Max Travel Time</th>
+									<th>Latest Arrival</th>
+									<th>Earliest Departure</th>
+								</tr>
+							</thead>
+							<tbody>
+								{processedResultsStorage.map((result, index) => (
+									<>
+										<tr key={index} onClick={() => toggleRowExpansion(index)}>
+											<td data-label="Meeting Date">
+												{extractDateTime(result.meetingTime)[0]}
+											</td>
+											<td data-label="Meeting Time">
+												{extractDateTime(result.meetingTime)[1]}
+											</td>
 
-										<td data-label="Min Travel Time">
-											{result.minTravelTimeInMinute}
-										</td>
-										<td data-label="Average Travel Time">
-											{result.averageTravelTimeInMinute}
-										</td>
-										<td data-label="Max Travel Time">
-											{result.maxTravelTimeInMinute}
-										</td>
-										<td data-label="Latest Arrival">
-											{result.latestArrival.split(".")[0]}
-										</td>
-										<td data-label="Earliest Departure">
-											{result.earliestDeparture.split(".")[0]}
-										</td>
-									</tr>
-									{/* Render the TravelDetails component when the row is expanded */}
-									{expandedRow === index && (
-										<tr key={-index}>
-											<td colSpan="9">
-												<DisplayDetailOfResults details={result} />
+											<td data-label="Min Travel Time">
+												{result.minTravelTimeInMinute}
+											</td>
+											<td data-label="Average Travel Time">
+												{result.averageTravelTimeInMinute}
+											</td>
+											<td data-label="Max Travel Time">
+												{result.maxTravelTimeInMinute}
+											</td>
+											<td data-label="Latest Arrival">
+												{result.latestArrival.split(".")[0]}
+											</td>
+											<td data-label="Earliest Departure">
+												{result.earliestDeparture.split(".")[0]}
 											</td>
 										</tr>
-									)}
-								</>
-							))}
-						</tbody>
-					</table>
-				</div>
+										{/* Render the TravelDetails component when the row is expanded */}
+										{expandedRow === index && (
+											<tr key={-index}>
+												<td colSpan="9">
+													{/* Flex container to show details and report side by side */}
+													<div className="container">
+														{/* Display details on the left */}
+														<div className="details">
+															<DisplayDetailOfResults details={result} />
+														</div>
+
+														{/* Display report on the right */}
+														<div className="report">
+															<ReportMaker arrayOfReport={processedReport} />
+														</div>
+													</div>
+												</td>
+											</tr>
+										)}
+									</>
+								))}
+							</tbody>
+						</table>
+					</div>
+				)}
 			</div>
 
 			<div>
 				<Visualise travelData={processedResultsStorage} />
-			</div>
-			<div>
-				<ReportMaker arrayOfReport={processedReport} />
 			</div>
 		</>
 	);
