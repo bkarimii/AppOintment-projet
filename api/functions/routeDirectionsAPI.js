7e716aea31141f7555e4749ea88ae83417dbd189
karimi <behrouzkarimi1993@gmail.com>
2024-10-25 16:48:51 +0100

    use static data

--- api/functions/routeDirectionsAPI.js
+++ api/functions/routeDirectionsAPI.js
@@ -10,50 +10,52 @@ export async function computeRoutesForOrigins(
 	fields,
 	apiKey,
 ) {
-	const meetingTimePromises = meetingTimeArray.map(async (arrivalTime) => {
-		const arrivalDetail = {
-			arrivalTime: arrivalTime,
-			details: [],
-		};
+	const runTheFetch = false;
+	if (runTheFetch) {
+		const meetingTimePromises = meetingTimeArray.map(async (arrivalTime) => {
+			const arrivalDetail = {
+				arrivalTime: arrivalTime,
+				details: [],
+			};
 
-		const originPromises = arrayOfOrigins.map(async (originElement) => {
-			try {
-				const requestBody = {
-					origin: {
-						location: {
-							latLng: originElement.location.latLng,
-						},
-					},
-					destination: {
-						location: {
-							latLng: {
-								latitude: destinationParam.latitude,
-								longitude: destinationParam.longitude,
+			const originPromises = arrayOfOrigins.map(async (originElement) => {
+				try {
+					const requestBody = {
+						origin: {
+							location: {
+								latLng: originElement.location.latLng,
 							},
 						},
-					},
-					travelMode,
-					arrivalTime,
-				};
-
-				const response = await axios.post(
-					"https://routes.googleapis.com/directions/v2:computeRoutes",
-					requestBody,
-					{
-						headers: {
-							"Content-Type": "application/json",
-							"X-Goog-Api-Key": apiKey,
-							"X-Goog-FieldMask": fields,
+						destination: {
+							location: {
+								latLng: {
+									latitude: destinationParam.latitude,
+									longitude: destinationParam.longitude,
+								},
+							},
 						},
-					},
-				);
+						travelMode,
+						arrivalTime,
+					};
 
-				if (response.data.routes && response.data.routes.length > 0) {
-					const route = response.data.routes[0];
-					let firstDepartureTime = null;
-					let lastArrivalTime = null;
-					let duration = null;
-					let staticDuration = null;
+					const response = await axios.post(
+						"https://routes.googleapis.com/directions/v2:computeRoutes",
+						requestBody,
+						{
+							headers: {
+								"Content-Type": "application/json",
+								"X-Goog-Api-Key": apiKey,
+								"X-Goog-FieldMask": fields,
+							},
+						},
+					);
+
+					if (response.data.routes && response.data.routes.length > 0) {
+						const route = response.data.routes[0];
+						let firstDepartureTime = null;
+						let lastArrivalTime = null;
+						let duration = null;
+						let staticDuration = null;
 
 						for (const leg of route.legs) {
 							duration = leg.duration || duration;
@@ -72,33 +74,36 @@ export async function computeRoutesForOrigins(
 							}
 						}
 
+						return {
+							city: originElement.city,
+							arrivalTime,
+							firstDepartureTime,
+							lastArrivalTime,
+							duration,
+							staticDuration,
+						};
+					} else {
+						return {
+							city: originElement.city,
+							error: "No routes found",
+						};
+					}
+				} catch (error) {
 					return {
 						city: originElement.city,
-						arrivalTime,
-						firstDepartureTime,
-						lastArrivalTime,
-						duration,
-						staticDuration,
-					};
-				} else {
-					return {
-						city: originElement.city,
-						error: "No routes found",
+						error: "Failed to compute the route",
 					};
 				}
-			} catch (error) {
-				return {
-					city: originElement.city,
-					error: "Failed to compute the route",
-				};
-			}
+			});
+
+			const details = await Promise.all(originPromises);
+			arrivalDetail.details.push(...details);
+			return arrivalDetail;
 		});
 
-		const details = await Promise.all(originPromises);
-		arrivalDetail.details.push(...details);
-		return arrivalDetail;
-	});
-
-	const userTravelInfoContainer = await Promise.all(meetingTimePromises);
-	return userTravelInfoContainer;
+		const userTravelInfoContainer = await Promise.all(meetingTimePromises);
+		return userTravelInfoContainer;
+	} else {
+		return fakeData;
+	}
 }
