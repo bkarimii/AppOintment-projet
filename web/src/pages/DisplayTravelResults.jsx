682c6f1b311f5171d44bdb9bd68fee9ff6efa9c7
karimi <behrouzkarimi1993@gmail.com>
2024-10-28 23:26:06 +0000

    Commited with a undef es-lint error

--- web/src/pages/DisplayTravelResults.jsx
+++ web/src/pages/DisplayTravelResults.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable no-undef */
+
 import {
 	faExclamationTriangle,
 	faCheckCircle,
 } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 import { ReportMaker } from "./ReportMaker";
 import Visualise from "./Visualise";
@@ -16,25 +20,14 @@ function DisplayTravelResults() {
 	const [processedResultsStorage, setProcessedResultsStorage] = useState([]);
 	const [processedReport, setProcessedReport] = useState([]);
 	const [expandedRow, setExpandedRow] = useState(null);
-	const [loading, setLoading] = useState(false); // Loading page state
+	const [loading, setLoading] = useState(false);
 	const [selectedOption, setSelectedOption] = useState("default");
-
-	const rankingDescriptions = {
-		default: "Default sorting (original order)",
-		min: " Minimum Travel Time",
-		max: " Maximum Travel Time",
-		minSlack: " Minimum Slack in Travel Time",
-	};
-
-	// Display meaningful ranking message based on selected option
-	const getRankingDescription = (option) =>
-		rankingDescriptions[option] || "Default sorting";
 
 	const navigate = useNavigate();
 	const url = "/api/compute-route";
 
 	const fetchTravelData = async (URL) => {
-		setLoading(true); // Set loading to true when starting to fetch data
+		setLoading(true);
 		try {
 			const bodyData = localStorage.getItem("newMeetingData");
 
@@ -53,12 +46,12 @@ function DisplayTravelResults() {
 				setProcessedResultsStorage(result);
 				setProcessedReport(reports);
 			} else {
-				console.error("An error happened!", response.status.error);
+				console.error("An error occurred!", response.status.error);
 			}
 		} catch (error) {
 			console.error(error);
 		} finally {
-			setLoading(false); // Set loading to false when done
+			setLoading(false);
 		}
 	};
 
@@ -78,192 +71,185 @@ function DisplayTravelResults() {
 		return [date, time];
 	};
 
-	// Handle sorting when radio button changes
-	const handleOptionChange = (e) => {
-		setSelectedOption(e.target.value);
-	};
-
-	// Sort based on selectedOption
-	const sortedResults = [...processedResultsStorage].sort((a, b) => {
-		switch (selectedOption) {
-			case "min":
-				return a.minTravelTimeInMinute - b.minTravelTimeInMinute;
-			case "max":
-				return a.maxTravelTimeInMinute - b.maxTravelTimeInMinute;
-			case "minSlack":
-				return a.arrivalSlack - b.arrivalSlack;
-			default:
-				return 0; // Default, no sorting
-		}
-	});
-
 	const toggleRowExpansion = (index) => {
-		setExpandedRow(expandedRow === index ? null : index); // Toggle the expanded row
+		setExpandedRow(expandedRow === index ? null : index);
 	};
 
 	const handleGoBackButton = (e) => {
 		e.preventDefault();
-		navigate("/new-meeting");
+		navigate("/");
+	};
+
+	function TableContent({ sortedResults }) {
+		return (
+			<table>
+				<thead>
+					<tr>
+						<th>Rank</th>
+						<th>Meeting Date</th>
+						<th>Meeting Time</th>
+						<th>Min Travel Time</th>
+						<th>Average Travel Time</th>
+						<th>Max Travel Time</th>
+						<th>Departures</th>
+						<th>Arrivals</th>
+						<th>Arrival Slack</th>
+						<th>Warnings</th>
+					</tr>
+				</thead>
+				<tbody>
+					{sortedResults.map((result, index) => (
+						<React.Fragment key={index}>
+							<tr onClick={() => toggleRowExpansion(index)}>
+								<td data-label="Rank"># {index + 1}</td>
+								<td data-label="Meeting Date">
+									{extractDateTime(result.meetingTime)[0]}
+								</td>
+								<td data-label="Meeting Time">
+									{extractDateTime(result.meetingTime)[1]}
+								</td>
+								<td data-label="Min Travel Time">
+									{result.minTravelTimeInMinute}
+								</td>
+								<td data-label="Average Travel Time">
+									{result.averageTravelTimeInMinute}
+								</td>
+								<td data-label="Max Travel Time">
+									{result.maxTravelTimeInMinute}
+								</td>
+								<td data-label="Departures">
+									{result.earliestDeparture} : {result.latestDeparture}
+								</td>
+								<td data-label="Arrivals">
+									{result.earliestArrival} : {result.latestArrival}
+								</td>
+								<td data-label="Arrival Slack">{result.arrivalSlack}</td>
+								<td data-label="Warnings">
+									{result.difficultTravels.length > 0 ||
+									result.tooLongTravels.length > 0 ? (
+										<FontAwesomeIcon
+											icon={faExclamationTriangle}
+											style={{ color: "orange", fontSize: "1.5em" }}
+											title="Warning"
+										/>
+									) : (
+										<FontAwesomeIcon
+											icon={faCheckCircle}
+											style={{ color: "green", fontSize: "1.5em" }}
+											title="All Good"
+										/>
+									)}
+								</td>
+							</tr>
+							{expandedRow === index && (
+								<tr>
+									<td colSpan="10">
+										<ReportMaker
+											timeOfReport={extractDateTime(result.meetingTime)[1]}
+											arrayOfReport={processedReport}
+										/>
+									</td>
+								</tr>
+							)}
+						</React.Fragment>
+					))}
+				</tbody>
+			</table>
+		);
+	}
+
+	const getSortedResults = () => {
+		const results = [...processedResultsStorage];
+		switch (selectedOption) {
+			case "min":
+				return results.sort(
+					(a, b) => a.minTravelTimeInMinute - b.minTravelTimeInMinute,
+				);
+			case "max":
+				return results.sort(
+					(a, b) => a.maxTravelTimeInMinute - b.maxTravelTimeInMinute,
+				);
+			case "minSlack":
+				return results.sort((a, b) => a.arrivalSlack - b.arrivalSlack);
+			default:
+				return results;
+		}
 	};
 
 	return (
 		<>
-			<button onClick={handleGoBackButton}>Go Back</button>
-			<div className="radio-buttons-container">
-				<legend>Rank the table by:</legend>
-				<div>
-					<input
-						type="radio"
-						id="default"
-						name="ranking"
-						value="default"
-						checked={selectedOption === "default"}
-						onChange={handleOptionChange}
-					/>
-					<label htmlFor="default">Default</label>
+			{loading ? (
+				<div className="spinner-container">
+					<div className="spinner"></div>
+					<p>Data is loading...</p>
 				</div>
+			) : (
+				<>
+					<button
+						onClick={handleGoBackButton}
+						aria-label="Go Back"
+						style={{ display: "flex", alignItems: "center" }}
+						id="back-button"
+					>
+						<FontAwesomeIcon
+							icon={faArrowLeft}
+							style={{ marginRight: "8px" }}
+						/>
+					</button>
+					<div className="table-container">
+						<Tabs
+							selectedTabClassName="active-tab"
+							onSelect={(index) => {
+								const options = ["default", "min", "max", "minSlack"];
+								setSelectedOption(options[index]);
+							}}
+						>
+							<TabList className="tabs">
+								<Tab className="tab">Default</Tab>
+								<Tab className="tab">Min Travel Time</Tab>
+								<Tab className="tab">Max Travel Time</Tab>
+								<Tab className="tab">Min Slack</Tab>
+							</TabList>
 
-				<div>
-					<input
-						type="radio"
-						id="min"
-						name="ranking"
-						value="min"
-						checked={selectedOption === "min"}
-						onChange={handleOptionChange}
-					/>
-					<label htmlFor="min">Min Travel Time</label>
-				</div>
-
-				<div>
-					<input
-						type="radio"
-						id="max"
-						name="ranking"
-						value="max"
-						checked={selectedOption === "max"}
-						onChange={handleOptionChange}
-					/>
-					<label htmlFor="max">Max Travel Time</label>
-				</div>
-
-				<div>
-					<input
-						type="radio"
-						id="minSlack"
-						name="ranking"
-						value="minSlack"
-						checked={selectedOption === "minSlack"}
-						onChange={handleOptionChange}
-					/>
-					<label htmlFor="minSlack">Min Slack Travel Time</label>
-				</div>
-
-				{/* Display the selected option */}
-				<p className="selected-option">
-					Table Ranked by: {getRankingDescription(selectedOption)}
-				</p>
-			</div>
-
-			<div>
-				{loading ? (
-					<p>
-						<strong>Loading data, please wait...</strong>
-					</p>
-				) : (
-					<div>
-						<table>
-							<thead>
-								<tr>
-									<th>Rank</th>
-									<th>Meeting Date</th>
-									<th>Meeting Time</th>
-									<th>Min Travel Time</th>
-									<th>Average Travel Time</th>
-									<th>Max Travel Time</th>
-									<th>Departures</th>
-									<th>Arrivals</th>
-									<th>Arrival Slack</th>
-									<th>Warnings</th>
-								</tr>
-							</thead>
-							<tbody>
-								{sortedResults.map((result, index) => (
-									<React.Fragment key={index}>
-										<tr
-											key={`main-row-${index}`}
-											onClick={() => toggleRowExpansion(index)}
-										>
-											<td data-label="Rank"># {index + 1}</td>
-											<td data-label="Meeting Date">
-												{extractDateTime(result.meetingTime)[0]}
-											</td>
-											<td data-label="Meeting Time">
-												{extractDateTime(result.meetingTime)[1]}
-											</td>
-											<td data-label="Min Travel Time">
-												{result.minTravelTimeInMinute}
-											</td>
-											<td data-label="Average Travel Time">
-												{result.averageTravelTimeInMinute}
-											</td>
-											<td data-label="Max Travel Time">
-												{result.maxTravelTimeInMinute}
-											</td>
-											<td data-label="Departure">
-												{result.earliestDeparture} : {result.latestDeparture}
-											</td>
-											<td data-label="Arrivals">
-												{result.earliestArrival} : {result.latestArrival}
-											</td>
-											<td data-label="Arrival Slack">{result.arrivalSlack}</td>
-											<td data-label="Warnings">
-												{result.difficultTravels.length > 0 ||
-												result.tooLongTravels.length > 0 ? (
-													<FontAwesomeIcon
-														icon={faExclamationTriangle}
-														style={{ color: "orange", fontSize: "1.5em" }}
-														title="Warning"
-														data-tip="Warning: you have long travel"
-													/>
-												) : (
-													<FontAwesomeIcon
-														icon={faCheckCircle}
-														style={{ color: "green", fontSize: "1.5em" }}
-														title="All Good"
-													/>
-												)}
-											</td>
-										</tr>
-										{expandedRow === index && (
-											<tr key={`expanded-row-${index}`}>
-												<td colSpan="9">
-													<div className="container">
-														<div className="report">
-															<ReportMaker
-																timeOfReport={
-																	extractDateTime(result.meetingTime)[1]
-																}
-																arrayOfReport={processedReport}
-															/>
-														</div>
-													</div>
-												</td>
-											</tr>
-										)}
-									</React.Fragment>
-								))}
-							</tbody>
-						</table>
+							<TabPanel>
+								<TableContent sortedResults={getSortedResults()} />
+							</TabPanel>
+							<TabPanel>
+								<TableContent sortedResults={getSortedResults()} />
+							</TabPanel>
+							<TabPanel>
+								<TableContent sortedResults={getSortedResults()} />
+							</TabPanel>
+							<TabPanel>
+								<TableContent sortedResults={getSortedResults()} />
+							</TabPanel>
+						</Tabs>
 					</div>
-				)}
-			</div>
-
-			<div>
-				<Visualise travelData={processedResultsStorage} />
-			</div>
+					<div>
+						<Visualise travelData={processedResultsStorage} />
+					</div>
+				</>
+			)}
 		</>
 	);
 }
 
 export default DisplayTravelResults;
+
+TableContent.propTypes = {
+	sortedResults: PropTypes.arrayOf(
+		PropTypes.shape({
+			meetingTime: PropTypes.string.isRequired, // assuming it's an ISO string
+			minTravelTimeInMinute: PropTypes.number.isRequired,
+			averageTravelTimeInMinute: PropTypes.number.isRequired,
+			maxTravelTimeInMinute: PropTypes.number.isRequired,
+			earliestDeparture: PropTypes.string.isRequired,
+			latestDeparture: PropTypes.string.isRequired,
+			earliestArrival: PropTypes.string.isRequired,
+			latestArrival: PropTypes.string.isRequired,
+			arrivalSlack: PropTypes.number.isRequired,
+			difficultTravels: PropTypes.array.isRequired,
+			tooLongTravels: PropTypes.array.isRequired,
+		}),
+	).isRequired,
+};
