7d0b2909632e7586d2e2f7183a868d47de828313
fikretellek <fikretellek@icloud.com>
2024-10-31 23:36:55 +0000

    meeting time conflict solved, GMT-BST to UTC functionality applied to time slot generating function

--- api/functions/timeRangeMaker.js
+++ api/functions/timeRangeMaker.js
@@ -1,14 +1,41 @@
-export function generateTimeSlots(startTime, endTime, intervalMinutes) {
+export function generateTimeSlots(
+	meetingDate,
+	startTime,
+	endTime,
+	intervalMinutes,
+) {
 	const slots = [];
-	const start = new Date(startTime);
-	const end = new Date(endTime);
+
+	const isBST = (date) => {
+		const marchDate = new Date(date.getFullYear(), 2, 31);
+		const marchLastSunday = new Date(
+			marchDate.setDate(marchDate.getDate() - marchDate.getDay()),
+		);
+		marchLastSunday.setHours(1, 0, 0, 0);
+
+		const octoberDate = new Date(date.getFullYear(), 9, 31);
+		const octoberLastSunday = new Date(
+			octoberDate.setDate(octoberDate.getDate() - octoberDate.getDay()),
+		);
+		octoberLastSunday.setHours(1, 0, 0, 0);
+
+		return date >= marchLastSunday && date < octoberLastSunday;
+	};
+
+	const start = new Date(`${meetingDate}T${startTime}`);
+	const end = new Date(`${meetingDate}T${endTime}`);
 
 	intervalMinutes = Number(intervalMinutes);
 	let current = new Date(start);
 
-	while (current <= end) {
-		slots.push(current.toISOString());
+	const formatToRFC3339 = (date) => {
+		const bstOffset = isBST(date) ? -60 : 0;
+		const utcDate = new Date(date.getTime() + bstOffset * 60 * 1000);
+		return utcDate.toISOString();
+	};
 
+	while (current <= end) {
+		slots.push(formatToRFC3339(current));
 		current.setMinutes(current.getMinutes() + intervalMinutes);
 	}
 
