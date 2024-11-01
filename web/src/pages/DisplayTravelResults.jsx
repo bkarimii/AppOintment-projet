aa6b6ba9b7e83a13dfa43b312a894e6be8a6255c
karimi <behrouzkarimi1993@gmail.com>
2024-11-01 11:53:09 +0000

    navigate to form page

--- web/src/pages/DisplayTravelResults.jsx
+++ web/src/pages/DisplayTravelResults.jsx
@@ -213,7 +213,7 @@ function DisplayTravelResults() {
 
 	const handleGoBackButton = (e) => {
 		e.preventDefault();
-		navigate("/");
+		navigate("/new-meeting");
 	};
 
 	const getSortedResults = () => {
