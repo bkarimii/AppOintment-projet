c98e7e8426475198a193b17075ecb927b5424260
karimi <behrouzkarimi1993@gmail.com>
2024-11-02 17:40:54 +0000

    Home page spec tests deleted - homepage replaced with meeting form

--- e2e/tests/about.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("can be visited", async ({ page }) => {
-	await page.goto("/nested/about/path");
-
-	await expect(page.getByRole("heading", { level: 1 })).toHaveText("About");
-});
